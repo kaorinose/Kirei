ae0c9888f334baf8be07ce6db59804bd41b3aafc
kaorinose <qbs003@gmail.com>
2020-09-08 17:31:52 +0900

    Initial Commit

---
+++ Kirei/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kirei
+//
+//  Created by 0003 QBS on 2020/09/08.
+//  Copyright © 2020 0003.kaori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
