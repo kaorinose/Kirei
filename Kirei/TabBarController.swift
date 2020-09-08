d72f634c85c4ca90044afa38f81c113f44a301aa
kaorinose <qbs003@gmail.com>
2020-09-08 21:29:01 +0900

    画面初回作成

---
+++ Kirei/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Kirei
+//
+//  Created by 0003 QBS on 2020/09/08.
+//  Copyright © 2020 0003.kaori. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
