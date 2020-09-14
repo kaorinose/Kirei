bb21e3f19d7d732b302e091eb0fc111108053abc
kaorinose <qbs003@gmail.com>
2020-09-14 16:25:39 +0900

    ログイン画面表示

--- Kirei/TabBarController.swift
+++ Kirei/TabBarController.swift
@@ -12,19 +12,16 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.90, blue: 0.90, alpha: 1)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // ログインしていないときの処理
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
     }
-    */
 
 }
